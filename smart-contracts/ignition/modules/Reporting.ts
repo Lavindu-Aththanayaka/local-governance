17d613b1060804974ca023ce4882339280991f3e
MalithaJeewaka <144432834+MalithaJeewaka@users.noreply.github.com>
2026-06-09 19:19:18 +0530

    feat: implement and deploy AuthorityMultiSig contract with reporting integration

--- smart-contracts/ignition/modules/Reporting.ts
+++ smart-contracts/ignition/modules/Reporting.ts
@@ -1,6 +1,20 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-export default buildModule("Reporting", (m) => {
+export default buildModule("ReportingAndMultiSig", (m) => {
   const reporting = m.contract("Reporting");
-  return { reporting };
+
+  const initialSuperAdmins = [
+    m.getAccount(0),
+    m.getAccount(1),
+    m.getAccount(2),
+    m.getAccount(3),
+    m.getAccount(4),
+  ];
+
+  const authorityMultiSig = m.contract("AuthorityMultiSig", [initialSuperAdmins, reporting]);
+
+  // Transfer ownership of Reporting to AuthorityMultiSig
+  m.call(reporting, "transferOwnership", [authorityMultiSig]);
+
+  return { reporting, authorityMultiSig };
 });
